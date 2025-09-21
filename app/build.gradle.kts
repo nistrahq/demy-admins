986981b9141ceeb1955a87ba0119f63d754eab59
Salim Ramirez <salimramirezm@gmail.com>
2025-09-20 19:33:42 -0500

    build(dependencies): add androidx.compose.ui.text library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.ui.text.google.fonts)
+    implementation(libs.androidx.compose.ui.text)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
