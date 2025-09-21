a76ae224d6c2afa32a951317c22f83bb881b7501
Salim Ramirez <salimramirezm@gmail.com>
2025-09-20 20:11:55 -0500

    feat(main): refactor MainActivity to use navigation graph and remove Greeting composable

--- app/src/main/java/com/nistra/demy/admins/MainActivity.kt
+++ app/src/main/java/com/nistra/demy/admins/MainActivity.kt
@@ -4,44 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.nistra.demy.admins.core.design.theme.DemyTheme
+import com.nistra.demy.admins.core.navigation.RootNavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DemyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Demy()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+private fun Demy() {
+    val navController = rememberNavController()
     DemyTheme {
-        Greeting("Android")
+        RootNavGraph(navController = navController)
     }
 }
