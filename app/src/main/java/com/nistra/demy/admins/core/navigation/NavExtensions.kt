d540ee70d1e2c6364168838bbe8291f91d5daa8d
Salim Ramirez <salimramirezm@gmail.com>
2025-10-05 23:46:51 -0500

    feat(core): enhance navigateOnce function with popUpTo support and overload for Destination

--- app/src/main/java/com/nistra/demy/admins/core/navigation/NavExtensions.kt
+++ app/src/main/java/com/nistra/demy/admins/core/navigation/NavExtensions.kt
@@ -2,10 +2,25 @@ package com.nistra.demy.admins.core.navigation
 
 import androidx.navigation.NavController
 
-fun NavController.navigateOnce(route: String) {
+fun NavController.navigateOnce(
+    route: String,
+    popUpToRoute: String? = null,
+    inclusive: Boolean = false
+) {
     this.navigate(route) {
         launchSingleTop = true
+        restoreState = true
+        if (popUpToRoute != null) {
+            popUpTo(popUpToRoute) {
+                saveState = true
+                this.inclusive = inclusive
+            }
+        }
     }
+}
+
+fun NavController.navigateOnce(destination: Destination) {
+    navigateOnce(destination.route)
 }
 
 fun NavController.popToRoot() {
