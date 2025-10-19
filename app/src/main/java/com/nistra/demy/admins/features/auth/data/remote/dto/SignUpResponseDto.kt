c0cfb36a72be6d83884039b5cc6484dbee0b510e
Salim Ramirez <salimramirezm@gmail.com>
2025-10-19 16:13:20 -0500

    feat(auth): remove tenantId from SignUpResponseDto for simplification

--- app/src/main/java/com/nistra/demy/admins/features/auth/data/remote/dto/SignUpResponseDto.kt
+++ app/src/main/java/com/nistra/demy/admins/features/auth/data/remote/dto/SignUpResponseDto.kt
@@ -3,6 +3,5 @@ package com.nistra.demy.admins.features.auth.data.remote.dto
 data class SignUpResponseDto(
     val id: String,
     val emailAddress: String,
-    val roles: List<String>,
-    val tenantId: String
+    val roles: List<String>
 )
