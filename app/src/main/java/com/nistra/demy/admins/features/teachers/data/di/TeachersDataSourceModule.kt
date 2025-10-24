a8ed0f5580b9fc56e7cbb543d3b03a3c45cd26f2
Salim Ramirez <salimramirezm@gmail.com>
2025-10-24 16:35:16 -0500

    feat(teachers): add Dagger modules for remote data source and API integration

---
+++ app/src/main/java/com/nistra/demy/admins/features/teachers/data/di/TeachersDataSourceModule.kt
@@ -0,0 +1,16 @@
+package com.nistra.demy.admins.features.teachers.data.di
+
+import com.nistra.demy.admins.features.teachers.data.datasource.remote.TeachersRemoteDataSource
+import com.nistra.demy.admins.features.teachers.data.datasource.remote.TeachersRemoteDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class TeachersDataSourceModule {
+    @Binds @Singleton
+    abstract fun bindTeachersRemoteDataSource(impl: TeachersRemoteDataSourceImpl): TeachersRemoteDataSource
+}
---
+++ app/src/main/java/com/nistra/demy/admins/features/teachers/data/di/TeachersRemoteDataModule.kt
@@ -0,0 +1,18 @@
+package com.nistra.demy.admins.features.teachers.data.di
+
+import com.nistra.demy.admins.features.teachers.data.remote.api.TeachersApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TeachersRemoteDataModule {
+    @Provides @Singleton
+    fun provideTeachersApi(retrofit: Retrofit): TeachersApi {
+        return retrofit.create(TeachersApi::class.java)
+    }
+}
