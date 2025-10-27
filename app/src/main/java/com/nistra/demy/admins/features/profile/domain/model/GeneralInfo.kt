18096ab139685a680cee1a219ebe527fe8f23866
iconicmiau <u202224135@gmail.com>
2025-10-27 17:25:57 -0500

    add initial sctructure and remote data model

---
+++ app/src/main/java/com/nistra/demy/admins/features/profile/domain/model/GeneralInfo.kt
@@ -0,0 +1,9 @@
+package com.nistra.demy.admins.features.profile.domain.model
+
+data class GeneralInfo(
+    val name: String,
+    val lastName: String,
+    val dni: String,
+    val email: String,
+    val phone: String,
+)
