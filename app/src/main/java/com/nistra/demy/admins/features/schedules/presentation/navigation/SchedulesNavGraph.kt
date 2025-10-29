a541ca554cce3e34c629fcebbe89b2e617122a34
Daniel <u20221g120@upc.edu.pe>
2025-10-28 21:44:44 -0500

    feat(schedules): add navigation graph and destinations for schedules feature

---
+++ app/src/main/java/com/nistra/demy/admins/features/schedules/presentation/navigation/SchedulesNavGraph.kt
@@ -0,0 +1,24 @@
+package com.nistra.demy.admins.features.schedules.presentation.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.nistra.demy.admins.features.main.presentation.navigation.MainDestination
+import com.nistra.demy.admins.features.schedules.presentation.ui.ScheduleViewerScreen
+import com.nistra.demy.admins.features.schedules.presentation.ui.SchedulesScreen
+
+fun NavGraphBuilder.schedulesGraph(navController: NavHostController) {
+    navigation(
+        startDestination = SchedulesDestination.Overview.route,
+        route = MainDestination.Schedules.route
+    ) {
+        composable(SchedulesDestination.Overview.route) {
+            SchedulesScreen()
+        }
+
+        composable(SchedulesDestination.ScheduleViewer.route) {
+            ScheduleViewerScreen()
+        }
+    }
+}
