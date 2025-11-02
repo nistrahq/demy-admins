bb4b9616997caa023a964151922267edcf33923e
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-01 23:41:39 -0500

    feat(billing): add api service

---
+++ app/src/main/java/com/nistra/demy/admins/features/billing/data/remote/api/BillingAccountsApi.kt
@@ -0,0 +1,20 @@
+package com.nistra.demy.admins.features.billing.data.remote.api
+
+import com.nistra.demy.admins.features.billing.data.remote.dto.BillingAccountResourceDto
+import com.nistra.demy.admins.features.billing.data.remote.dto.CreateBillingAccountRequestDto
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface BillingAccountsApi {
+
+    @GET("billing-accounts")
+    suspend fun getAllBillingAccounts(): List<BillingAccountResourceDto>
+
+    @GET("billing-accounts/{billingAccountId}")
+    suspend fun getBillingAccountById(@Path("billingAccountId") billingAccountId: String ): BillingAccountResourceDto
+
+    @POST("billing-accounts")
+    suspend fun createBillingAccount(@Body request: CreateBillingAccountRequestDto): BillingAccountResourceDto
+}
