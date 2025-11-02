73e20a218e8e13bc6d8c165ae35ca956a0c119ee
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-01 23:41:06 -0500

    feat(billing): add mapper

---
+++ app/src/main/java/com/nistra/demy/admins/features/billing/data/mapper/BillingAccountMapper.kt
@@ -0,0 +1,26 @@
+package com.nistra.demy.admins.features.billing.data.mapper
+
+import com.nistra.demy.admins.features.billing.data.remote.dto.BillingAccountResourceDto
+import com.nistra.demy.admins.features.billing.data.remote.dto.CreateBillingAccountRequestDto
+import com.nistra.demy.admins.features.billing.domain.model.BillingAccount
+
+fun BillingAccountResourceDto.toDomain(): BillingAccount {
+    val accountId = this.id
+
+    return BillingAccount (
+        id = this.id,
+        studentId = this.studentId,
+        invoices = this.invoices.map { it.toDomain(billingAccountId = accountId) },
+        academyId = this.academyId
+
+    )
+}
+
+
+fun BillingAccount.toRequestDto(): CreateBillingAccountRequestDto {
+    return CreateBillingAccountRequestDto(
+        studentId = this.studentId
+    )
+}
+
+
