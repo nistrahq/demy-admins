5acef424c7bf110e23ed7e761aaa59ca20619206
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-04 23:07:55 -0500

    feat(billing): update domain model

--- app/src/main/java/com/nistra/demy/admins/features/billing/domain/model/BillingAccount.kt
+++ app/src/main/java/com/nistra/demy/admins/features/billing/domain/model/BillingAccount.kt
@@ -1,5 +1,7 @@
 package com.nistra.demy.admins.features.billing.domain.model
 
+import com.nistra.demy.admins.features.invoicing.domain.model.Invoice
+
 data class BillingAccount(
     val id: String,
     val studentId: String,
