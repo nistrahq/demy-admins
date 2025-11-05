576419d4b83f11cd88881872232251459cb37016
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-04 23:11:59 -0500

    feat(billing): update repository implementation

--- app/src/main/java/com/nistra/demy/admins/features/billing/data/repositories/BillingAccountRepositoryImpl.kt
+++ app/src/main/java/com/nistra/demy/admins/features/billing/data/repositories/BillingAccountRepositoryImpl.kt
@@ -1,10 +1,14 @@
 package com.nistra.demy.admins.features.billing.data.repositories
 
 import com.nistra.demy.admins.features.billing.data.datasource.remote.BillingAccountRemoteDataSource
-import com.nistra.demy.admins.features.billing.data.mapper.toDomain
+import com.nistra.demy.admins.features.billing.data.mapper.toDomain as toBillingAccountDomain
+import com.nistra.demy.admins.features.invoicing.data.mapper.toResourceDto
 import com.nistra.demy.admins.features.billing.data.mapper.toRequestDto
 import com.nistra.demy.admins.features.billing.domain.model.BillingAccount
 import com.nistra.demy.admins.features.billing.domain.repository.BillingAccountRepository
+import com.nistra.demy.admins.features.invoicing.data.mapper.toResourceDto
+import com.nistra.demy.admins.features.invoicing.domain.model.Invoice
+
 import javax.inject.Inject
 
 class BillingAccountRepositoryImpl @Inject constructor(
@@ -13,16 +17,25 @@ class BillingAccountRepositoryImpl @Inject constructor(
 
     override suspend fun  getAllBillingAccounts(): List<BillingAccount> {
         val billingAccountsResponse = billingAccountsRemoteDataSource.fetchBillingAccounts()
-        return billingAccountsResponse.map { it.toDomain() }
+        return billingAccountsResponse.map { it.toBillingAccountDomain() }
     }
 
     override suspend fun getBillingAccountById(billingAccountId: String): BillingAccount {
         val billingAccountResponse = billingAccountsRemoteDataSource.fetchBillingAccountById(billingAccountId)
-        return billingAccountResponse.toDomain()
+        return billingAccountResponse.toBillingAccountDomain()
     }
 
     override suspend fun  createBillingAccount(billingAccount: BillingAccount): BillingAccount {
         val createdBillingAccountResourceDto = billingAccountsRemoteDataSource.addBillingAccount(billingAccount.toRequestDto())
-        return  createdBillingAccountResourceDto.toDomain()
+        return  createdBillingAccountResourceDto.toBillingAccountDomain()
+    }
+
+    override suspend fun addInvoiceToBillingAccount(billingAccountId: String, invoice: Invoice): BillingAccount {
+        val invoiceRequestDto = invoice.toResourceDto()
+
+        val updatedBillingAccountDto = billingAccountsRemoteDataSource.addInvoiceToBillingAccount(billingAccountId, invoiceRequestDto)
+
+        return updatedBillingAccountDto.toBillingAccountDomain()
+
     }
 }
