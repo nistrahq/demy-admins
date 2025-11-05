41ff18cef08d1a907865ee5d85214e2cdb9fd5fc
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-04 23:14:35 -0500

    feat(billing): update destination

--- app/src/main/java/com/nistra/demy/admins/features/billing/presentation/navigation/BillingDestination.kt
+++ app/src/main/java/com/nistra/demy/admins/features/billing/presentation/navigation/BillingDestination.kt
@@ -1,11 +1,20 @@
 package com.nistra.demy.admins.features.billing.presentation.navigation
 
+const val BILLING_ACCOUNT_ID_ARG = "billingAccountId"
 sealed interface BillingDestination {
     val route: String
 
     data object Billing : BillingDestination {
         override val route = "billing"
         fun toRoute() = route
+    }
+    data object BillingAccountDetails: BillingDestination {
+
+        override val route = "billing_details"
+
+        fun toRoute() = "$route/{$BILLING_ACCOUNT_ID_ARG}"
+
+        fun createRoute(billingAccountId: String) = "$route/$billingAccountId"
     }
 }
 
