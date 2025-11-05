0b5c2cebf44cdde9976a7ab9280e1a9f343eff30
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-04 23:14:43 -0500

    feat(billing): update dto

--- app/src/main/java/com/nistra/demy/admins/features/billing/data/remote/dto/BillingAccountResourceDto.kt
+++ app/src/main/java/com/nistra/demy/admins/features/billing/data/remote/dto/BillingAccountResourceDto.kt
@@ -1,5 +1,7 @@
 package com.nistra.demy.admins.features.billing.data.remote.dto
 
+import com.nistra.demy.admins.features.invoicing.data.remote.dto.InvoiceResourceDto
+
 data class BillingAccountResourceDto(
     val id: String,
     val studentId: String,
