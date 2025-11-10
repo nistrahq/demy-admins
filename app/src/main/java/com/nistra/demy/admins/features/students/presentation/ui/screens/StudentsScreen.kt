a752d08d267cdb8aa8bf9740e98b1849b766f410
Kyrubi <u20221c486@upc.edu.pe>
2025-11-10 16:24:24 -0500

    feat(students): add screen header to StudentsScreen

--- app/src/main/java/com/nistra/demy/admins/features/students/presentation/ui/screens/StudentsScreen.kt
+++ app/src/main/java/com/nistra/demy/admins/features/students/presentation/ui/screens/StudentsScreen.kt
@@ -1,17 +1,20 @@
 package com.nistra.demy.admins.features.students.presentation.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.nistra.demy.admins.R
 import com.nistra.demy.admins.features.students.presentation.ui.components.RegisterStudent
 import com.nistra.demy.admins.features.students.presentation.ui.components.StudentList
+import com.nistra.demy.admins.features.students.presentation.ui.components.StudentsHeader
 import com.nistra.demy.admins.features.students.presentation.viewmodel.StudentsViewModel
 
 @Composable
@@ -21,31 +24,41 @@ fun StudentsScreen(
     val uiState by viewModel.uiState.collectAsState()
     val formData by viewModel.formData.collectAsState()
 
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    Column (
+       modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        RegisterStudent (
-            modifier = Modifier.weight(0.50f),
-            studentToEdit = uiState.studentToEdit,
-            formData = formData,
-            isLoading = uiState.isLoading,
-            errorMessage = uiState.errorMessage,
-            onFormChange = viewModel::onStudentFormChange,
-            onSaveStudentClick = viewModel::onSaveStudentClick,
-            onClearFormClick = viewModel::onClearFormClick
+        StudentsHeader(
+            title = stringResource(R.string.students_screen_title),
+            description = stringResource(R.string.students_screen_description)
         )
 
-        StudentList(
-            modifier = Modifier.weight(0.50f),
-            students = uiState.students,
-            onStudentSelected = viewModel::onStudentSelectedForEdit,
-            onDeleteStudent = viewModel::onDeleteStudentClick ,
-            onSearchQueryChange = viewModel::searchStudents,
-            searchQuery = uiState.searchQuery
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            RegisterStudent (
+                modifier = Modifier.weight(0.50f),
+                studentToEdit = uiState.studentToEdit,
+                formData = formData,
+                isLoading = uiState.isLoading,
+                errorMessage = uiState.errorMessage,
+                onFormChange = viewModel::onStudentFormChange,
+                onSaveStudentClick = viewModel::onSaveStudentClick,
+                onClearFormClick = viewModel::onClearFormClick
+            )
+
+            StudentList(
+                modifier = Modifier.weight(0.50f),
+                students = uiState.students,
+                onStudentSelected = viewModel::onStudentSelectedForEdit,
+                onDeleteStudent = viewModel::onDeleteStudentClick ,
+                onSearchQueryChange = viewModel::searchStudents,
+                searchQuery = uiState.searchQuery
+            )
+        }
     }
 }
 
