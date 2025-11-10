f4c93c998fb1a83d15795b5dd6db33e32e0d8a4b
iconicmiau <u202224135@gmail.com>
2025-11-10 17:59:00 -0500

    feat: add new distribution for profile presentation

---
+++ app/src/main/java/com/nistra/demy/admins/features/profile/presentation/ui/components/InlineValue.kt
@@ -0,0 +1,4 @@
+package com.nistra.demy.admins.features.profile.presentation.ui.components
+
+class InlineValue {
+}
---
+++ app/src/main/java/com/nistra/demy/admins/features/profile/presentation/ui/screens/ProfileInfoCard.kt
@@ -0,0 +1,4 @@
+package com.nistra.demy.admins.features.profile.presentation.ui.screens
+
+class ProfileInfoCard {
+}
