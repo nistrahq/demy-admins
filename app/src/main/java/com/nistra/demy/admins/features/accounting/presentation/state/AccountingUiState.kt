c20e39eee97ab6f0d671256c5722ce8d2f4b01a2
Salim Ramirez <salimramirezm@gmail.com>
2025-11-11 09:27:55 -0500

    feat(accounting): add AccountingUiState for managing transactions UI state

---
+++ app/src/main/java/com/nistra/demy/admins/features/accounting/presentation/state/AccountingUiState.kt
@@ -0,0 +1,34 @@
+package com.nistra.demy.admins.features.accounting.presentation.state
+
+import com.nistra.demy.admins.features.accounting.domain.model.Transaction
+
+/**
+ * UI state for the accounting transactions list screen.
+ *
+ * @property transactions List of all transactions.
+ * @property filteredTransactions List of transactions after applying search filter.
+ * @property searchQuery Current search query.
+ * @property isLoading Whether data is being loaded.
+ * @property errorMessage Error message to display, if any.
+ * @property isEditDialogOpen Whether the edit dialog is open.
+ * @property transactionBeingEdited The transaction currently being edited.
+ * @property isSavingEdit Whether a save operation is in progress.
+ * @property isDeleteDialogOpen Whether the delete confirmation dialog is open.
+ * @property transactionToDelete The ID of the transaction to delete.
+ * @property isDeletingTransaction Whether a delete operation is in progress.
+ * @author Salim Ramirez
+ */
+data class AccountingUiState(
+    val transactions: List<Transaction> = emptyList(),
+    val filteredTransactions: List<Transaction> = emptyList(),
+    val searchQuery: String = "",
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val isEditDialogOpen: Boolean = false,
+    val transactionBeingEdited: Transaction? = null,
+    val isSavingEdit: Boolean = false,
+    val isDeleteDialogOpen: Boolean = false,
+    val transactionToDelete: String? = null,
+    val isDeletingTransaction: Boolean = false
+)
+
