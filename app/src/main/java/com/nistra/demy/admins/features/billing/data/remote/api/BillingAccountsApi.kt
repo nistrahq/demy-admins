6205026dbac0a4a6c8987197228e8dd39fc586f7
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-12 04:10:53 -0500

    feat(billing): update api

--- app/src/main/java/com/nistra/demy/admins/features/billing/data/remote/api/BillingAccountsApi.kt
+++ app/src/main/java/com/nistra/demy/admins/features/billing/data/remote/api/BillingAccountsApi.kt
@@ -4,6 +4,7 @@ import com.nistra.demy.admins.features.billing.data.remote.dto.BillingAccountRes
 import com.nistra.demy.admins.features.billing.data.remote.dto.CreateBillingAccountRequestDto
 import com.nistra.demy.admins.features.invoicing.data.remote.dto.InvoiceResourceDto
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -29,4 +30,16 @@ interface BillingAccountsApi {
         @Path("billingAccountId") billingAccountId: String,
         @Path("invoiceId") invoiceId: String
     ): InvoiceResourceDto
+
+
+    @GET("billing-accounts/invoices/by-student-dni/{dniNumber}")
+    suspend fun getInvoicesByStudentDni(
+        @Path("dniNumber") dniNumber: String
+    ): List<InvoiceResourceDto>
+
+    @DELETE("billing-accounts/{billingAccountId}/invoices/{invoiceId}")
+    suspend fun deleteInvoice(
+        @Path("billingAccountId") billingAccountId: String,
+        @Path("invoiceId") invoiceId: String
+    )
 }
